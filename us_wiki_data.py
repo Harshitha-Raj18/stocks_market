4a45090cae40c93f11d72a4349aa4d241d89e370
Harshitha Raj <harshitha@curlanalytics.com>
2017-06-14 09:51:47 +0530

    addded data
    stocks

--- us_wiki_data.py
+++
@@ -1,8 +0,0 @@
-import quandl
-quandl.ApiConfig.api_key = 'cZ_QsSLJCuVgr3cyz5__'
-data = quandl.get_table('WIKI/PRICES', date = { 'gte': '2000-01-01', 'lte': '2017-04-21' })
-print(data.tail())
-# Taking only one asset
-A_table = data[data["ticker"]=="A"]
-# Finding unique assets
-data["ticker"].unique()
